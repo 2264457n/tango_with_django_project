bb01ed9570dfcd8ab0a1499b243b8c7d240620da
Kara <2264457n@student.gla.ac.uk>
2018-01-25 15:52:27 +0000

    half way through chapter 5

--- rango/models.py
+++ rango/models.py
@@ -3,4 +3,17 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+    def _str_(self): #For Python 2, use _unicode_ too
+        return self.name
+
+class Page(models.Model):
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def _str_(self): #For Python 2, use _unicode_ too
+        return self.name
